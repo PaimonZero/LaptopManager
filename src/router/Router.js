35bd5c99356c1efc27fe679208c92319ab0883c2
QE180086 <145005942+QE180086@users.noreply.github.com>
2024-11-03 22:26:21 +0700

    update function

--- src/router/Router.js
+++ src/router/Router.js
@@ -12,6 +12,10 @@ const ProductDetailPage = lazy(
 
 const ProductListPage = lazy(() => import('../pages/products/ProductListPage'));
 const AddProductPage = lazy(() => import('../pages/products/AddProductPage'));
+const UpdateProductPage = lazy(
+	() => import('../pages/products/UpdateProductPage'),
+);
+
 
 
 export const router = createBrowserRouter([
@@ -53,6 +57,14 @@ export const router = createBrowserRouter([
 		element: (
 			<Suspense fallback={<LoadingComponent />}>
 				<ProductDetailPage />
+			</Suspense>
+		),
+	},
+	{
+		path: '/hextech/update-product/:id',
+		element: (
+			<Suspense fallback={<LoadingComponent />}>
+				<UpdateProductPage />
 			</Suspense>
 		),
 	},
