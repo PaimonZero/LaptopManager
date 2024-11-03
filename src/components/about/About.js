46dc341187ad0c04f5ec37fb76b35afd9ac8d50d
trungnguyenngoc123 <137521550+trungnguyenngoc123@users.noreply.github.com>
2024-11-03 22:59:39 +0700

    Update About.js

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -7,7 +7,7 @@ const About = () => {
 			<div className="container d-flex flex-column align-items-center justify-content-center mt-5 mb-5">
 				<div className="row align-items-center justify-content-center">
 					<div className="col-md-4 d-flex flex-column align-items-center justify-content-center">
-						{/* <img src={favicon} alt="LabTrack Logo" className="img-fluid" /> */}
+						<img src='/images/logo512.png' alt="Logo" className="img-fluid" />
 					</div>
 					<div className="col-md-6 align-items-center justify-content-center">
 						<p className="lead">
@@ -25,12 +25,12 @@ const About = () => {
 					</div>
 				</div>
 			</div>
-			<div className="container d-flex flex-column align-items-center justify-content-center mt-2">
-				<h6 className="display-6 mb-4">Thành viên</h6>
-				<a href="https://github.com/PaimonZero/LaptopManager/graphs/contributors">
-					<img src="https://contrib.rocks/image?repo=PaimonZero/LaptopManager" />
-				</a>
-			</div>
+			// <div className="container d-flex flex-column align-items-center justify-content-center mt-2">
+			// 	<h6 className="display-6 mb-4">Thành viên</h6>
+			// 	<a href="https://github.com/PaimonZero/LaptopManager/graphs/contributors">
+			// 		<img src="https://contrib.rocks/image?repo=PaimonZero/LaptopManager" />
+			// 	</a>
+			// </div>
 		</div>
 	);
 };
